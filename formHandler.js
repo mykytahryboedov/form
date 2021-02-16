9b72c9ad9910b5ebbdaa8a027936eeb0cfceed65
mykytahryboedov <realsmokeman@gmail.com>
2021-02-16 17:59:56 +0200

    .

--- formHandler.js
+++ formHandler.js
@@ -1,4 +1,24 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const surname = document.getElementById('surname')
+const names = document.getElementsByClassName('names')
+
+
+const reg = new RegExp('[АБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгдеєжзиіїйклмнопрстуфхцчшщьюя]');
+
+// function validateEmail(email){
+//     let reg =  /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+//     if (reg.test(email.value) == false) {
+//         console.log('1234')
+//         return false;
+        
+//     }
+// }
+
+function insertAttr(){
+    const arr = [...names]
+    arr.forEach(e => {
+        e.setAttribute('pattern','[АБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯабвгдеєжзиіїйклмнопрстуфхцчшщьюя]')
+    })
+    
+}
+
+insertAttr()
