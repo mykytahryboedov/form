8805906c3d7e5cb7ab37551ef308292c016b7cd1
mykytahryboedov <realsmokeman@gmail.com>
2021-02-17 17:57:18 +0200

    everything is going bad

--- formHandler.js
+++ formHandler.js
@@ -1,5 +1,34 @@
 const surname = document.getElementById('surname')
 const names = document.getElementsByClassName('names')
+const familyCounterInput = document.getElementById('familyCounter')
+const familyList = document.getElementById('familyMemberList')
+const acceptCountButton = document.getElementById('familyCounterButton');
+
+acceptCountButton.addEventListener('click', generateList)
+
+function generateList(){
+    if (familyList.innerHTML == '') {
+        for (let i = 1; i <= familyCounterInput.value; i++) {
+            familyList.insertAdjacentHTML(
+                'beforeend', `
+            <li class="familyMemberListItem">
+                          <p class="description" id="listItemTitle${i + familyCounterInput.value}" >№${i} РНОКПП <span class="neccessary">*</span></p>
+                          <input type="text" name="familyItems" pattern="[0-9]{10,10}" required>
+                        </li>`
+                        )
+          }
+         
+    }
+
+
+    else {
+        familyList.innerHTML.replace(/\s/g , "")
+            for (let i = 0; i <= familyList.children.length; i--) {
+                
+                familyList.removeChild(document.querySelector('li'))
+            }      
+}
+}
 
 
 // const checkBox = document.querySelector('input[type="checkbox"]')
@@ -25,5 +54,9 @@ function insertAttr(){
     })
     
 }
+
+
+
+
 
 insertAttr()
